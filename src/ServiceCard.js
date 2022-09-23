aa61b7e7092d1c9b6fc72c16ca4f3c4ca7eedef6
Federico Cometto <fedecometto98@gmail.com>
2022-09-23 15:43:13 -0300

    ultimoooo

--- src/ServiceCard.js
+++ src/ServiceCard.js
@@ -3,8 +3,11 @@ import Contenedor from './components/Contenedor'
 function App() {
   return (
   
-    <div className="container d-flex">
+    <div className="container d-flex bg-success p-4 align-items-center justify-content-center vh-100">
+      <div className='container d-flex bg-danger'>
+        
         <Contenedor />
+      </div>
                  
     </div> 
   );
