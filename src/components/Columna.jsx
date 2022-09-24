3a413baebdc3a63db0cb82513835e89485088aaa
Federico Cometto <fedecometto98@gmail.com>
2022-09-24 13:00:41 -0300

    ultimo

--- src/components/Columna.jsx
+++ src/components/Columna.jsx
@@ -3,22 +3,6 @@ import React, { Fragment } from 'react'
 import Formulario from './Formulario'
 
 const Columna = () => {
-<<<<<<< HEAD
-
-    
-
-    return (
-        <Fragment>
-
-            <div className="">
-                <div className="">
-                    
-                    <Formulario />
-                </div>
-            
-            </div>
-
-=======
     return (
         <Fragment>
             <div className="">
@@ -26,7 +10,6 @@ const Columna = () => {
                     <Formulario />
                 </div>
             </div>
->>>>>>> 4df73acf902a4629eaf61c92a2332c4462d48d01
         </Fragment>
       );
 }
